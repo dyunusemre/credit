7b4511605cea2db16b8703460439b54cca966f5a
dyunusemre <dirilyunusemre@gmail.com>
2023-10-01 14:37:48 +0300

    refactoring

--- src/main/java/com/example/credit/domain/credit/GetCreditBalanceHandler.java
+++ src/main/java/com/example/credit/domain/credit/GetCreditBalanceHandler.java
@@ -5,13 +5,14 @@ import com.example.credit.domain.common.model.DomainComponent;
 import com.example.credit.domain.common.model.OperationResult;
 import com.example.credit.domain.common.model.OperationStatus;
 import com.example.credit.domain.credit.model.Amount;
+import com.example.credit.domain.credit.model.Credit;
 import com.example.credit.domain.credit.port.CreditPort;
 import com.example.credit.domain.creditoperation.usecase.query.GetCreditBalance;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Optional;
 
-import static com.example.credit.domain.common.utils.CreditUtils.calculateAvailableTotalAmount;
+import static com.example.credit.domain.common.utils.CreditUtils.calculateTotalAmount;
 
 @DomainComponent
 @RequiredArgsConstructor
@@ -21,7 +22,7 @@ public class GetCreditBalanceHandler implements UseCaseHandler<OperationResult<A
     @Override
     public OperationResult<Amount> handle(GetCreditBalance useCase) {
         var availableCredits = creditPort.findAllActiveCreditByUserId(useCase.userId());
-        var balance = calculateAvailableTotalAmount(availableCredits);
+        var balance = calculateTotalAmount(availableCredits.stream().map(Credit::getAmount).toList());
         return new OperationResult<>(Optional.of(balance), OperationStatus.SUCCESS);
     }
 }
